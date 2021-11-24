a4741d7900b82c851ce72b3524b968c6d5454c12
VictoriaBozzola <mvbozzola@gmail.com>
2021-11-24 18:11:43 -0300

    css nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+// import foto from "../../assets/img/Persona1.png";
+import './Nav.css';
+import {Link} from 'react-router-dom';
+
 
 export default function Nav(){
     return(
-
-        <h1>HOLA</h1>
-
+        <div className='containerNav'>
+            <h1 className='nombreLogo'>ATECHNAS</h1>
+            <SearchBar></SearchBar>
+            <Link className='linkNavReg' to=''>
+                <h3>Ingresar / Registrarse</h3> 
+            </Link>
+       </div>
     )
 }
