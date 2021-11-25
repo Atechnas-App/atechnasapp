cd8235ac6fc377b2888df037a233f333b5c37c22
olarteandres <andresolarte@gmail.com>
2021-11-25 15:38:31 -0300

    tabla tecnologias

--- api/src/models/Technology.js
+++ api/src/models/Technology.js
@@ -11,140 +11,145 @@ module.exports = (sequelize) => {
       primaryKey: true
     },
 
-    python: {
+    technology: {
       type: DataTypes.STRING,
-      allowNull: true,
-    },
+      allowNull: false,
+  },
 
-    java: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // python: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    javaScript: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // java: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    C: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // javaScript: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    PHP: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // C: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    typeScript: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // PHP: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    Swift: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // typeScript: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    angular: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // Swift: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    C: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // angular: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    groovy: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // C: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    assemblyLanguage: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // groovy: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    SQL: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
+    // assemblyLanguage: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    swift: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // SQL: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: true,
+    // },
 
-    Go: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // swift: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    prisma: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+    // Go: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    Ilustrator: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // prisma: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true,
+    // },
 
-    Photoshop: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // Ilustrator: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
 
-    Indesign: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // Photoshop: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
 
-    corelDraw: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // Indesign: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
 
-    gravitDesigner: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // corelDraw: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
 
-    hubSpot: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // gravitDesigner: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
 
-    activeCampaign: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // hubSpot: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
 
-    klaviyo: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // activeCampaign: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
 
-    mailchimp: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // klaviyo: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
 
-    marketo: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // mailchimp: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
 
-    pardot: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // marketo: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
 
-    omnisend: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+    // pardot: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
+
+    // omnisend: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //   },
 
   });
 };
