f3bff4b9643db671f7a1ec92f58dfadadc0b62be
Erick-Rangel <rebel863@hotmail.com>
2021-11-25 07:57:13 -0600

    link home y login

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -8,9 +8,11 @@ import {Link} from 'react-router-dom';
 export default function Nav(){
     return(
         <div className='containerNav'>
+            <Link to="/">
             <h1 className='nombreLogo'>ATECHNAS</h1>
+            </Link>
             <SearchBar></SearchBar>
-            <Link className='linkNavReg' to=''>
+            <Link className='linkNavReg' to='/login'>
                 <h3>Ingresar / Registrarse</h3> 
             </Link>
        </div>
