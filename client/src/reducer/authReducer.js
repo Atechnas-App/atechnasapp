d40fb959510a812f9bd36579cb5b9c1198693060
Erick-Rangel <rebel863@hotmail.com>
2021-11-26 16:48:08 -0600

    arreglo de google, rutas

--- client/src/reducer/authReducer.js
+++ client/src/reducer/authReducer.js
@@ -5,11 +5,8 @@ export const authReducer =(state={},action)=>{
     switch(action.types){
     case types.login:
             return{
-                uid: action.payload.type,
-                name: action.payload.displayName,
-                email: action.payload.email,
-                photo: action.payload.photoURL,
-
+                ...action.payload,
+                
             }
             case types.logout:
                 return {}
