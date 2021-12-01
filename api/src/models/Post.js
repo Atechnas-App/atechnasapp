721a42fffed3e51a084196d534b333a293b87d9d
olarteandres <andresolarte@gmail.com>
2021-12-01 15:16:53 -0300

    model post listo

--- api/src/models/Post.js
+++ api/src/models/Post.js
@@ -10,17 +10,10 @@ module.exports = (sequelize) => {
       allowNull: false,
       primaryKey: true
     },
-<<<<<<< HEAD
 
     title: {
       type: DataTypes.STRING,
       allowNull: true,
-=======
-    // el id user deberia ser mediante tabla intermedia, cambié todo a true ara probar la db
-    idUser: {
-        type: DataTypes.STRING,
-        allowNull: true,
->>>>>>> 7a24bb4b175cf397dab1aef29cfcae9d4d76cf6a
     },
 
     image: {
@@ -36,14 +29,12 @@ module.exports = (sequelize) => {
     description: {
       type: DataTypes.STRING,
       allowNull: true,
-<<<<<<< HEAD
     },
-
+    
     qualification: {
       type: DataTypes.STRING,
       allowNull: true,
-=======
->>>>>>> 7a24bb4b175cf397dab1aef29cfcae9d4d76cf6a
+
     },
 
   });
