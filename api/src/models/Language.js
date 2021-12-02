064aa34f9b4b0e549602fd7f08ff490068b11a03
Lucas Matias <59143253+LukeMatias@users.noreply.github.com>
2021-12-02 10:39:32 -0300

    search.js working with pagination

--- api/src/models/Language.js
+++ api/src/models/Language.js
@@ -1,20 +1,21 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('language', {
-    id:{
+  sequelize.define("language", {
+    id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       allowNull: false,
-      primaryKey: true
+      primaryKey: true,
     },
 
     languages: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
 
+    // timestamps: false,
   });
 };
