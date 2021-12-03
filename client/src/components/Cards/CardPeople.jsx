876b1f2257796ad62f2baa5f093e6ef4f6de4df4
VictoriaBozzola <mvbozzola@gmail.com>
2021-12-03 10:24:02 -0300

    css Cards

--- client/src/components/Cards/CardPeople.jsx
+++ client/src/components/Cards/CardPeople.jsx
@@ -4,8 +4,8 @@ import './CardPeople.css'
 export default function CardPeople({profilePicture,lastName, name, technology,qualification, id, categories}){
     console.log('CARD INFO', `${name} ${profilePicture} ${lastName} category: ${categories} Technology: ${technology}`)
     return(
-        <div>
-            <div className='contenedorCard'>
+        <div className= 'contenedorCard border-card btn btn-white btn-bottom-animation-1 btn-animation-1'>
+            <div>
                 <div className='contenedor-img-card'>
                 <img src={profilePicture} alt="img not found" className='imgCard' />
                 </div>
@@ -14,7 +14,9 @@ export default function CardPeople({profilePicture,lastName, name, technology,qu
                 <p className='technologies'>{technology? technology?.map((e) => e) : 
                 'Javascript | React | Readux | Node.js'}</p>
                 <p>{qualification}</p>
+                <div>
                 <Link to={"/" + id}><button className='botonDetalleCard'>Mas Detalles</button></Link>
+                </div>
             </div>
         </div>
     )
