9175f2ad6d0483d929972357679df4a90cf07fbb
VictoriaBozzola <mvbozzola@gmail.com>
2021-12-05 22:43:46 -0300

    carrucel con css

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ export default function Home(){
             <div className='ajustarHome'> 
                          
                 <Nav/>
-                <Carrucel/>
+                <Carrucel className='carrucel'/>
                 <PageInfo/>
                 <Presentation/>
                 <Testimonial/>
