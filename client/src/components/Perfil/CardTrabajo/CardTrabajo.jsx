c74de8def72b625f71791c59add87480ca6de909
gongoody <gonzaagodoy12@gmail.com>
2021-12-07 17:23:21 -0300

    animacion cardTrabajo

--- client/src/components/Perfil/CardTrabajo/CardTrabajo.jsx
+++ client/src/components/Perfil/CardTrabajo/CardTrabajo.jsx
@@ -4,23 +4,40 @@ import { getJobs } from "../../../actions/actions"
 import "./CardTrabajo.css"
 
 export default function CardTrabajo(id){
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(getJobs(id))
-    },[dispatch,id])
-    const jobs = useSelector((state)=> state.rootReducer.jobs)
-    console.log(jobs)
-    return(<>
-        {jobs && jobs.map((j) =>{
-            var qual = ""
-            for(var i = 0; i < j.qualification; i++){
-                qual += "•"
-            }
-        return (<div className="trabajo-container">
-            <h2 className="trabajo-titulo">{j.title}</h2>
-            <h3 className="trabajo-empresa">{j.company}</h3>
-            <h3 className="trabajo-url">{j.url}</h3>
-            <h3 className="trabajo-calificacion">{qual} {j.qualification}.0</h3>            
-        </div>)})}
-    </>)
+    // const dispatch = useDispatch()
+    // useEffect(() => {
+    //     dispatch(getJobs(id))
+    // },[dispatch,id])
+    // const jobs = useSelector((state)=> state.rootReducer.jobs)
+    // console.log(jobs)
+    // return(<>
+    //     {jobs && jobs.map((j) =>{
+    //         var qual = ""
+    //         for(var i = 0; i < j.qualification; i++){
+    //             qual += "•"
+    //         }
+    //     return (<div className="trabajo-container">
+    //         <h2 className="trabajo-titulo">Titulo</h2>
+    //         <h3 className="trabajo-precio">Precio</h3>
+                     
+    //     </div>)})}
+    // </>)
+
+    return (
+        
+        <div className="trabajo-container">
+            <div className="the-card">
+                <div className="front-card">
+                    <h1>Titulo</h1>
+                    <h2>Precio</h2>
+                </div>
+
+                <div className="back-card">
+                    <button>Mas detalles</button>
+                    <button>Contratar</button>
+                </div>
+            </div>     
+        </div>
+
+        )
 }
