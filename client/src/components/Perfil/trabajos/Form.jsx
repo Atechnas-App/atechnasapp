a959a18fa9d6c9eceaaa1aa16f3dd94889551ff1
Erick-Rangel <rebel863@hotmail.com>
2021-12-07 14:08:29 -0600

    form trabajos

---
+++ client/src/components/Perfil/trabajos/Form.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export const Form = () => {
+    return (
+        <div>
+            <form>
+                <div>
+                    <label>Titulo</label>
+                    <input type="text" name="titulo" />
+                    <img src="/" alt=""/>
+                    <button>Agregar</button>
+                    <img src="/" alt=""/>
+                    <button>Agregar</button>
+                    <img src="/" alt=""/>
+                    <button>Agregar</button>
+                    <img src="/" alt=""/>
+                    <button>Agregar</button>
+                    <img src="/" alt=""/>
+                    <button>Agregar</button>
+                    <img src="/" alt=""/>
+                    <button>Agregar</button>
+                    
+                    <label>Descripcion</label>
+                    <textarea name="descripcion" />
+
+                    <label>Precio</label>
+                    <input type="number" name="precio" />
+
+                    <button>Guardar</button>
+
+                </div>
+                </form>            
+
+
+
+
+
+
+        </div>
+    )
+}
