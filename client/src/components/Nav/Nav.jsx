95d3a00b7daca5411e389cb120225442a6b09bb0
Erick-Rangel <rebel863@hotmail.com>
2021-12-09 07:22:33 -0600

    agregue editar perfil a la imagen

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,55 +1,54 @@
-import React, { useEffect } from 'react';
-import SearchBar from '../SearchBar/SearchBar';
+import React, { useEffect } from "react";
+import SearchBar from "../SearchBar/SearchBar";
 // import foto from "../../assets/img/Persona1.png";
-import './Nav.css';
-import {Link , useHistory} from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { getDetails, logoutAll } from '../../actions/actions';
+import "./Nav.css";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getDetails, logoutAll } from "../../actions/actions";
 
-
-export default function Nav(){
-  
+export default function Nav(props) {
+  let fullId = props?.match?.params?.id;
   const detail = useSelector((state) => state.rootReducer.details);
-   const history = useHistory()
- const dispatch = useDispatch()
- if (!localStorage.getItem("user")) {
-   localStorage.setItem("user", "{}");
-} 
+  const history = useHistory();
+  const dispatch = useDispatch();
+  if (!localStorage.getItem("user")) {
+    localStorage.setItem("user", "{}");
+  }
 
+  const name = localStorage.getItem("displayName");
+  const photo = localStorage.getItem("photoURL");
+  const user = JSON.parse(localStorage.getItem("user"));
 
-   const name = localStorage.getItem("displayName")
-   const photo = localStorage.getItem("photoURL")
-   const user = JSON.parse(localStorage.getItem("user")); 
- 
-   let name1 = user.name;
-   let id = user.id;
-   
-   useEffect(() => {
-     dispatch(getDetails(id));
-    }, [dispatch]);
-    
-    let photo1  = detail.profilePicture?  detail.profilePicture:user.profilePicture ;
-    
-   const handleLogout = () => {
-       dispatch(logoutAll());
-       localStorage.setItem("user", "{}")
-       history.push("/login");     
-   }
-    
-    return (
-      <div className="containerNav">
-        <Link to="/">
-          <h1 className="nombreLogo">ATECHNAS</h1>
+  let name1 = user.name;
+  let id = user.id;
+
+  useEffect(() => {
+    dispatch(getDetails(id));
+  }, [dispatch]);
+
+  let photo1 = detail.profilePicture
+    ? detail.profilePicture
+    : user.profilePicture;
+
+  const handleLogout = () => {
+    dispatch(logoutAll());
+    localStorage.setItem("user", "{}");
+    history.push("/login");
+  };
+
+  return (
+    <div className="containerNav">
+      <Link to="/">
+        <h1 className="nombreLogo">ATECHNAS</h1>
+      </Link>
+      <SearchBar></SearchBar>
+
+      {!name && !name1 ? (
+        <Link className="linkNavReg" to="/login">
+          <h3>Ingresar / Registrarse</h3>
         </Link>
-        <SearchBar></SearchBar>
-
-        {!name && !name1 ? (
-          <Link className="linkNavReg" to="/login">
-            <h3>Ingresar / Registrarse</h3>
-          </Link>
-        ) : (
-          <div className="containerUser">
-            <div className="containerUserImg">
+      ) : (
+        <div className="containerUser">
               <img
                 className="imgUser"
                 src={photo ? photo : photo1}
@@ -57,18 +56,28 @@ export default function Nav(){
                 width="100vw"
                 heigth="100vh"
               />
-              <h3 >{name ? name : name1}</h3>
-            </div>
-            <div >
+              <h3>{name ? name : name1}</h3>
+            
+
+            <div className="containerUserImg">
+          <ul className="submenu">
+            <li>
               <Link to={"/perfil/" + id}>
-                <button style={{textDecoration:"none"}}>Perfil</button>
+                <button style={{ textDecoration: "none" }}>Perfil</button>
               </Link>
-            </div>
-            <div>
+            </li>
+            <li>
+              <Link to={`/perfil/edit/${id}`}>
+                <button>Editar Perfil</button>
+              </Link>
+            </li>
+            <li>
               <button onClick={handleLogout}>cerrar sesion</button>
-            </div>
+            </li>
+      </ul>
           </div>
-        )}
-      </div>
-    );
+          </div>
+      )}
+    </div>
+  );
 }
