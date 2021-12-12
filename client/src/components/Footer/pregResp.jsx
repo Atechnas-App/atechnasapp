ed5778b3531d49955c0161de7cd33e6d3cbe89db
gongoody <gonzaagodoy12@gmail.com>
2021-12-12 05:31:50 -0300

    rutas footer terminadas

--- client/src/components/Footer/pregResp.jsx
+++ client/src/components/Footer/pregResp.jsx
@@ -1,9 +1,13 @@
 import './pregResp'
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
 
 export default function PregResp(){
     return(
         <div>
-            
+            <Nav/>
+            <h1>FAQ</h1>
+            <Footer/>
         </div>
     )
 }
--- client/src/components/Footer/terminosCondiciones.jsx
+++ client/src/components/Footer/terminosCondiciones.jsx
@@ -1,9 +1,13 @@
 import './terminosCondiciones'
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
 
 export default function TerminosCondiciones(){
     return(
         <div>
-            
+            <Nav/>
+            <h1>Terminos y Condiciones</h1>
+            <Footer/>
         </div>
     )
 }
