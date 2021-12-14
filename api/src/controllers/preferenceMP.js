1c6ae6385491848cc5beb7715a9271a443783903
Tomas <tomasignaciocontreras@hotmail.com>
2021-12-14 01:31:57 -0300

    flujo de pagos completo, ahora guarda info transaccion y redirige al Home. Agregado modelo Payments. Falta relacionar

--- api/src/controllers/preferenceMP.js
+++ api/src/controllers/preferenceMP.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const router = express.Router();
 const mercadopago = require('mercadopago')
-const { User } = require('../db')
+const { User, Payments } = require('../db')
 const axios = require('axios')
 
 
 
 
 router.post("/create_preference", async (req, res) => {
-    const prestador = await User.findByPk('ffe41a85-adcd-4474-8c9d-3a1968d052b1', { raw: true})
-    mercadopago.configure({
-        access_token: prestador.access_token,
-    });
-    const {quantity, price, description} = req.body
+	const prestador = await User.findByPk('ca1e5b8a-8502-4794-8936-64a545524dfb', { raw: true })
+	mercadopago.configure({
+		access_token: prestador.access_token,
+	});
+	const { quantity, price, description } = req.body
 
 	let preference = {
 		items: [
@@ -23,12 +23,12 @@ router.post("/create_preference", async (req, res) => {
 			}
 		],
 		back_urls: {
-			"success": "http://localhost:3000",
-			"failure": "http://localhost:3000",
-			"pending": "http://localhost:3000"
+			"success": "http://localhost:3001/api/feedback",
+			"failure": "http://localhost:3001/api/feedback",
+			"pending": "http://localhost:3001/api/feedback"
 		},
 		auto_return: "approved",
-		marketplace_fee : 5
+		marketplace_fee: 5
 	};
 	await mercadopago.preferences.create(preference)
 		.then(function (response) {
@@ -38,12 +38,38 @@ router.post("/create_preference", async (req, res) => {
 		});
 });
 
-router.get('/feedback', function(req, res) {
-	res.json({
-		Payment: req.query.payment_id,
-		Status: req.query.status,
-		MerchantOrder: req.query.merchant_order_id
-	});
+router.get('/feedback', async function (req, res) {
+	const { payment_id, status, payment_type, merchant_order_id, preference_id, site_id, processing_mode, merchant_account_id } = req.query
+	try {
+		await Payments.findOrCreate({
+			where: {
+				payment_id: payment_id,
+				status: status,
+				payment_type: payment_type,
+				merchant_order_id: merchant_order_id,
+				preference_id: preference_id,
+				site_id: site_id,
+				processing_mode: processing_mode,
+				merchant_account_id: merchant_account_id
+			}
+		})
+	}
+	catch(err){
+		console.log(err)
+	}
+
+	res.redirect('http://localhost:3000')
 });
 
 module.exports = router
+
+// query: {
+//     payment_id: '18819619701',
+//     status: 'approved',
+//     payment_type: 'credit_card',
+//     merchant_order_id: '3773850169',
+//     preference_id: '1034206289-0b769ba1-4d96-41a4-b82b-8ab5942cb754',
+//     site_id: 'MLA',
+//     processing_mode: 'aggregator',
+//     merchant_account_id: 'null'
+//   },
