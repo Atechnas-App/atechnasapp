1306142ff8097874726a56c92a5507888bfedb63
Erick-Rangel <rebel863@hotmail.com>
2021-12-13 19:29:04 -0600

    de nuevo

--- client/src/routes/InvitedRoute.js
+++ client/src/routes/InvitedRoute.js
@@ -2,22 +2,22 @@ import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { LoginadnRegister } from '../components/form/LoginadnRegister';
 import Home from '../components/Home/Home';
-/* import SearchPage from '../components/Searches/SearchPage'; */
-/* import PerfilPrivado from "../components/Perfil/PerfilPrivado"
+import SearchPage from '../components/Searches/SearchPage';
+import PerfilPrivado from "../components/Perfil/PerfilPrivado"
 import PerfilPublico from "../components/Perfil/PerfilPublico"
 import EditPerfil from '../components/EditPerfil/EditPerfil';
 import DetallesTrabajo from '../components/Perfil/Trabajos/DetallesTrabajo';
-/* import Form from '../components/Perfil/Trabajos/Form' 
-import EditarTrabajo from '../components/Perfil/Trabajos/EditarTrabajo'; */
-/* import Error404 from '../components/Error404/Error404'
+import Form from "../components/Perfil/Trabajos/Form";
+import EditarTrabajo from '../components/Perfil/Trabajos/EditarTrabajo';
+import Error404 from '../components/Error404/Error404'
 import Contactanos from '../components/Footer/Contactanos'
 import SobreNosotros from '../components/Footer/sobreNosotros'
 import PregResp from '../components/Footer/pregResp'
-import Terms from '../components/Footer/terminosCondiciones' */
+import Terms from '../components/Footer/terminosCondiciones'
 
 
 export const InvitedRoute = () => {
-  /* const local = JSON.parse(localStorage.getItem("user")); */
+  const local = JSON.parse(localStorage.getItem("user"));
   // const isAdmin = localStorage.getItem("isAdmin");
 
   return (
@@ -25,20 +25,20 @@ export const InvitedRoute = () => {
       {/* RUTAS PUBLICAS */}
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={LoginadnRegister} />
-      {/* <Route exact path="/results" component={SearchPage} />
+      <Route exact path="/results" component={SearchPage} />
       <Route exact path="/error" component={Error404} />
       <Route exact path="/sobreNosotros" component={SobreNosotros}/>
       <Route exact path="/FAQ" component={PregResp}/>
       <Route exact path="/contactanos" component={Contactanos}/>
       <Route exact path="/terms" component={Terms}/>
-      <Route exact path='/contactanos' component={Contactanos}/> */}
+      <Route exact path='/contactanos' component={Contactanos}/>
 
 
       {/* <Route path="/results?searcher=" component={SearchPage} /> */}
 
 
-        {/* {/* RUTAS USUARIOS */}
-       {/* {!local?.id && <Redirect exact from="/perfil/:id" to="/login" />}
+        {/* RUTAS USUARIOS */}
+        {!local?.id && <Redirect exact from="/perfil/:id" to="/login" />}
         {!local?.id && <Redirect from="/miPerfil/:id" to="/error" />}
         {!local?.id && <Redirect from="/perfil/editarPerfil/:id" to="/error" />}
         {!local?.id && <Redirect from="/trabajos/detalle/:id" to="/error" />}
@@ -50,9 +50,9 @@ export const InvitedRoute = () => {
         {local?.id && <Route exact path="/perfil/editarPerfil/:id" component={EditPerfil}/>}
         {!local?.id && <Route exact path="/login" component={LoginadnRegister} />}
         {local?.id && <Route exact path="/trabajos/detalle/:id" component={DetallesTrabajo}/>}
-        {/* {local?.id && <Route exact path="/perfil/crearTrabajos/:id" component={Form} />} 
-        {local?.id && <Route exact path="/perfil/editarTrabajos/:id" component={EditarTrabajo} />}
-         */}
+        {local?.id && <Route exact path="/perfil/crearTrabajos/:id" component={Form} />}
+        {local?.id && <Route exact path="/perfil/editarTrabajos/:id" component={EditarTrabajo} />} {/* agregar /:name/ */ }
+        
       </Switch>
     );
 }
