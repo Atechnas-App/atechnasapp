ccc2390103be672632392bd081b5061bce02b8e3
VictoriaBozzola <mvbozzola@gmail.com>
2021-12-14 21:38:25 -0300

    comentarios adm

--- client/src/components/Admin/Comentarios.jsx
+++ client/src/components/Admin/Comentarios.jsx
@@ -1,7 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { getUser} from '../../actions/actions';
+
 export default function Comentarios(){
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const users = useSelector((state)=> state.rootReducer.users)
+    const [comentarios, setComentarios] = useState({
+        
+    })
+
+    useEffect(() => {
+        dispatch(getUser())
+    },[dispatch])
+
+
+
     return(
         <div>
-            <h1>Comentarios</h1>
+            <div>
+                <h1>Empresa</h1>
+                <p>Comentario</p> 
+            </div>
+            <button>Eliminar</button>
         </div>
     )
 }
