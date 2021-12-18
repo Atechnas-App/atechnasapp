0b731ab125dd1033f651d8e2172d8053d79e552b
Erick-Rangel <rebel863@hotmail.com>
2021-12-18 14:46:49 -0600

    de nuevo

--- api/src/controllers/passport-config-github.js
+++ api/src/controllers/passport-config-github.js
@@ -58,10 +58,13 @@ passport.deserializeUser((user, done) => { done(null, user) })
 
 router.get('/github', passport.authenticate('github', { scope: ['profile'] }))
 
-router.get('/github/callback', passport.authenticate('github', {
-  successRedirect: GITHUB_CLIENT_URL,
-  failureRedirect: GITHUB_CLIENT_URL + '/login'
-}))
+router.get(
+  "https://atechnas-api.herokuapp.com/api/github/callback/github/callback",
+  passport.authenticate("github", {
+    successRedirect: GITHUB_CLIENT_URL,
+    failureRedirect: GITHUB_CLIENT_URL + "/login",
+  })
+);
 
 router.get('/login/success', (req, res) => {
   /* const name = info.name? info.name : 'ejemplo' */
